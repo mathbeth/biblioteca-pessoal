639e8478637b38c0f2f24545ea3697bcd17e69c0
mathbeth <matheus-nascimento.mn@academico.ifpb.edu.br>
2023-06-22 17:13:41 -0300

    novo: adição de ícone de lápis (futura implementação de update)

--- public/js/index.js
+++ public/js/index.js
@@ -1,117 +1,3 @@
-/* function addUsuario(usuario) {
-  fetch('/cadastro', {
-    method: 'POST',
-    body: JSON.stringify(usuario),
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  });
-}
-async function loadUsuarios() {
-  const response = await fetch('/usuarios');
-
-  const usuarios = await response.json();
-
-  for (const usuario of usuarios) {
-    addUsuario(usuario);
-  }
-}
-
-function loadFormSubmit() {
-  const form = document.querySelector('form');
-
-  form.onsubmit = async (event) => {
-    event.preventDefault();
-
-    const username = document.querySelector('#username').value;
-
-    const email = document.querySelector('#email').value;
-
-    const senha = document.querySelector('#senha').value;
-
-    const usuario = { username, email, senha };
-
-    const response = await fetch('/cadastro', {
-      method: 'POST',
-      body: JSON.stringify(usuario),
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
-
-    if (response.ok) {
-      const novoUsuario = await response.json();
-
-      addUsuario(novoUsuario);
-
-      form.reset();
-
-      document.querySelector('#submitForm').click();
-
-      alert('Usuário cadastrado com sucesso!');
-      
-      window.location.href = '/login';
-
-    } else {
-      alert('Erro ao cadastrar usuário!');
-    }
-    
-  };
-} */
-
-// A parte do login ainda não funciona
-function addLoginUsuario(usuario) {
-  fetch('/login', {
-    method: 'POST',
-    body: JSON.stringify(usuario),
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  });
-}
-
-function loadLoginFormSubmit() {
-  const form = document.getElementById('loginForm');
-
-  form.onsubmit = async (event) => {
-    event.preventDefault();
-    const email = document.querySelector('#email').value;
-
-    const senha = document.querySelector('#senha').value;
-
-    const usuario = { email, senha };
-
-    const response = await fetch('/login', {
-      method: 'POST',
-      body: JSON.stringify(usuario),
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
-    const novoLogin = await response.json();
-
-    if (response.ok) {
-      console.log(`${novoLogin.user_id}`);
-
-      localStorage.setItem('user_id', novoLogin.user_id);
-      localStorage.setItem('email', novoLogin.email);
-
-      const user_id = localStorage.getItem('user_id');
-      const email = localStorage.getItem('email');
-
-      console.log(`User ID armazenado: ${user_id}`);
-      console.log(`Email armazenado: ${email}`);
-    } else {
-      console.log('error');
-    }
-
-    form.reset();
-    document.querySelector('#submitForm').click();
-
-    window.location.href = "/perfil";
-  };
-}
-
 function getBookView(livro) {
   const { livro_id, titulo, autor, editora, genero } = livro;
 
@@ -128,7 +14,16 @@ function getBookView(livro) {
             >
             </span>
           </div>
+          <div class="icon-pencil" style="float: right; display: inline">
+          <span
+            class="iconify"
+            data-icon="ph:pencil"
+            style="font-size: 1.2rem"
+          >
+          </span>
         </div>
+        </div>
+
         <div class="card-body">
           <div>
             <span class="fw-bold">Autor:</span> ${autor}
@@ -156,11 +51,11 @@ async function addBookView(livro) {
 
   const iconTrash = livroCard.querySelector('.icon-trash');
   iconTrash.addEventListener('click', async () => {
-    await fetch(`/livros/${livro.livro_id}`, {
-      method: 'DELETE',
-    });
+   await fetch(`/livros/${livro.livro_id}`, {
+     method: 'DELETE',
+   });
 
-    livroCard.remove();
+   livroCard.remove();
   });
 }
 
@@ -174,5 +69,4 @@ async function loadBooks() {
   }
 }
 
-/* loadFormSubmit(); */
 loadBooks();
